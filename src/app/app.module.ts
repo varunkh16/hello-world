d3f7fa8c0c1912d073d94b9cae1cfb5271183f59
Varun Kodigehalli Hanumanth Raju -X (vkodigeh - INFOSYS LIMITED at Cisco) <vkodigeh@cisco.com>
2019-11-26 15:14:40 +0530

    commiting some sample files from angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { InputUserDataFormComponent } from './input-user-data-form/input-user-data-form.component';
+import { DisplayUserDataComponent } from './display-user-data/display-user-data.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InputUserDataFormComponent,
+    DisplayUserDataComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
